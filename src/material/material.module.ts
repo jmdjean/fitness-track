79b0e0c7b88c1e2fadd0c4f8070cd37b046cb1cd
SP01\jean.medes <jean.medes@totvs.com.br>
2025-12-15 17:32:38 -0300

    Building signup form

--- src/material/material.module.ts
+++ src/material/material.module.ts
@@ -1,9 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatCardModule, MatButtonModule],
+  exports: [CommonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatCardModule, MatButtonModule],
 })
 export class MaterialModule {}
